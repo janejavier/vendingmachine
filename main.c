049862b0950a08106c814ea97f0f76646aaf4b61
janejavier <jfj758@uowmail.edu.au>
2023-09-27 20:26:45 +0400

    my first commit

--- main.c
+++ main.c
@@ -1,5 +1,24 @@
-#include<stdio.h>
-int main(){
-printf("Hello Worldddddd \n"); 
-return 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define admin_pass ("password")
+#define MIN 5
+#define total_amount 
+
+int main(void)
+{
+char A = 5;
+char B = 10;
+char C = 15;
+char quit = "q";
+char currency = "DHS"
+char i = NULL;
+char status = "N";
+
+
+
+
+
 }
+    
