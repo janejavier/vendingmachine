00f0a915b586a6683715a723e5e918ae2bc7a9e2
janejavier <jfj758@uowmail.edu.au>
2023-09-28 12:59:03 +0400

    myfirst

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #define MIN 5
 #define total_amount 
 
-int main(void)
+int main()
 {
 char A = 5;
 char B = 10;
@@ -17,8 +17,12 @@ char i = NULL;
 char status = "N";
 
 
+printf("VENDING MACHINE");
+
+printf("Please Choose An item:  /n");
 
 
+return;
 
 }
     
