2351b69d4afd8b47b8e0ba8a2490ba5566737016
janejavier <jfj758@uowmail.edu.au>
2023-10-04 13:09:18 +0400

    myfirst

--- main.c
+++ main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 #define admin_pass ("password")
-#define MIN 5
-#define total_amount 
+#define MIN 5 // create alert if item chosen becomes less or equal to min
+#define total_amount // gives the total amount of sale since last reset.
 
+//task 1
 int main()
 {
 
@@ -15,10 +14,12 @@ printf("Please Choose An item: \n");
 int Option1 = 5;
 int Option2 = 10;
 int Option3 = 15;
-printf(" A = %d DHS \n", Option1);
-printf(" B = %d DHS \n", Option2);
-printf(" C = %d DHS \n", Option3);
+printf(" A = %d DHS \n", Option1); //choose name for A & store character
+printf(" B = %d DHS \n", Option2); //choose name for B & store character
+printf(" C = %d DHS \n", Option3); //choose name for C & store character
 
+
+//put if statement when user chooses an item (using scanf),if user inputs ... then... else...
 
 return 0;
 
